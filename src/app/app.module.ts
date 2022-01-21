c7b5c70a02381ed01d524f79d5434f694d74edae
ShubhamUjinwal <shubhamujinwal114@gmail.com>
2022-01-20 19:56:54 -0500

    added models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    ProductDetailsComponent,
+    HeaderComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -1,9 +1,26 @@
 export class Product {
-  blend_name!: string;
-  id!: number;
-  intensifier!: string;
-  notes!: string;
-  origin!: string;
-  uid!: string;
-  variety!: string;
+  blend_name: string;
+  id: number;
+  intensifier: string;
+  notes: string;
+  origin: string;
+  uid: string;
+  variety: string;
+
+  constructor(
+    blend_name: string,
+    id: number,
+    intensifier: string,
+    notes: string,
+    origin: string,
+    uid: string,
+    variety: string) {
+      this.blend_name = blend_name;
+      this.id = id;
+      this.intensifier = intensifier;
+      this.notes = notes;
+      this.origin = origin;
+      this.uid = uid;
+      this.variety = variety;
+  }
 }
